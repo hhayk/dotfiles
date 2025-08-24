bf4abc4691d3cdba9d071eae4ab506f8e4cf5ef5
Hayk Hakobyan <hayk.hakobyan87@gmail.com>
2025-08-24 14:35:39 -0400

    Key mapping for RustLsp explainError

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -10,4 +10,7 @@ map("i", "jj", "<ESC>")
 -- copilot
 vim.api.nvim_set_keymap('i', '<C-l>', 'copilot#Accept()', { expr = true, silent = true, noremap = false })
 
+-- rust-analizer
+map("n", "<C-l>", "<cmd> RustLsp explainError <cr>", { desc = "Explain Error" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
