5e9d1a642e3698ecae73535fe3981ffcf3366a96
Hayk Hakobyan <hayk.hakobyan87@gmail.com>
2025-08-24 14:37:20 -0400

    rust.lua LSP config for inlayHints

--- .config/nvim/lua/plugins/rust.lua
+++ .config/nvim/lua/plugins/rust.lua
@@ -2,6 +2,36 @@ return {
   {
     'mrcjkb/rustaceanvim',
     version = '^6', -- Recommended
+    init = function()
+      -- Configure rustaceanvim here
+      vim.g.rustaceanvim = {
+        -- Plugin configuration
+        tools = {
+        },
+        -- LSP configuration
+        server = {
+          on_attach = function(client, bufnr)
+            -- you can also put keymaps in here
+            --
+            -- Enable inlay hints for the current buffer
+            vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+          end,
+          default_settings = {
+            -- rust-analyzer language server configuration
+            ['rust-analyzer'] = {
+              inlayHints = {
+                -- typeHints = { enable = true },
+                -- parameterHints = { enable = true },
+                -- Other inlay hint settings can be added here
+              },
+            },
+          },
+        },
+        -- DAP configuration
+        dap = {
+        },
+      }
+    end,
     lazy = false, -- This plugin is already lazy
   },
   {
